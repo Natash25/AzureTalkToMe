f95911a8a79d3705eb28d3414d45b4684e80916e
Amy Mao <amy_m123@icloud.com>
2019-01-26 17:25:34 -0800

    set up

---
+++ build/precache-manifest.429ec9c93ba32fa9aa5f42a6286f7837.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "012072a5491779e0fae9",
+    "url": "/static/css/main.a54f410e.chunk.css"
+  },
+  {
+    "revision": "012072a5491779e0fae9",
+    "url": "/static/js/main.012072a5.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a994913cf262aa7b0f3596c49dc2d252",
+    "url": "/index.html"
+  }
+];
